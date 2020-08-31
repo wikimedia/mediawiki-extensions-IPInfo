fcbbfe02507e272ac651aff57993439bab48f9a3
Thalia <thalia.e.chan@googlemail.com>
2020-08-31 18:01:22 +0100

    Add extension.json and other required files
    Bug: T260609
    Change-Id: Ia42ea2e6d59dac0d200b7c0874dea2f5118a2255

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/* eslint-env node, es6 */
+
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true,
+				fix: grunt.option( 'fix' )
+			},
+			all: [
+				'*.{js,json}',
+				'modules/**/*.{js,json}'
+			]
+		},
+		stylelint: {
+			all: [
+				'*.{css,less}',
+				'modules/**/*.{css,less}'
+			]
+		},
+		banana: conf.MessagesDirs
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
