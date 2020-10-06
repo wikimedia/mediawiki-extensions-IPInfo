c2b9eeec8e130763234c94b8fa99514ec0607509
Thalia <thalia.e.chan@googlemail.com>
2020-10-06 16:32:59 +0100

    Display error returned from API in IPInfo widget
    Also display an error if the IP didn't match.
    
    Bug: T263409
    Change-Id: I4dd2d715422fd47688d85f07eed12f9bcbd17fb7

--- modules/ext.ipInfo/widget.js
+++ modules/ext.ipInfo/widget.js
@@ -47,29 +47,51 @@
 	mw.IpInfo.IpInfoWidget.prototype.success = function ( info ) {
 		var location, source;
 
-		// @TODO: display error if data is still undefined - T263409
-		if ( !info ) {
-			return;
+		if ( info ) {
+			location = info.location.map( function ( item ) {
+				return item.label;
+			} ).join( mw.msg( 'comma-separator' ) );
+			source = mw.msg( 'ipinfo-widget-source-mock' );
+
+			this.$element.append(
+				$( '<p>' ).addClass( 'ext-ipinfo-widget-location' ).text( location ),
+				$( '<p>' ).addClass( 'ext-ipinfo-widget-source' ).text( source )
+			);
+		} else {
+			// The IP address did not match the log or revision ID
+			this.displayError( mw.msg( 'ipinfo-widget-error-wrong-ip' ) );
 		}
-
-		location = info.location.map( function ( item ) {
-			return item.label;
-		} ).join( mw.msg( 'comma-separator' ) );
-		source = mw.msg( 'ipinfo-widget-source-mock' );
-
-		this.$element.append(
-			$( '<p>' ).addClass( 'ext-ipinfo-widget-location' ).text( location ),
-			$( '<p>' ).addClass( 'ext-ipinfo-widget-source' ).text( source )
-		);
 	};
 
 	/**
 	 * Failure callback for the info promise.
 	 *
-	 * @param {Object} info
+	 * @param {Object} error
 	 */
-	mw.IpInfo.IpInfoWidget.prototype.failure = function ( /* error */ ) {
-		// @TODO Display error
+	mw.IpInfo.IpInfoWidget.prototype.failure = function ( error ) {
+		if ( error.responseJSON && error.responseJSON.messageTranslations ) {
+			this.displayError( error.responseJSON.messageTranslations[
+				mw.config.get( 'wgContentLanguage' )
+			] );
+		} else {
+			this.displayError();
+		}
+	};
+
+	/**
+	 * Display an error, given an error message.
+	 *
+	 * @param {string} [label] Error message
+	 */
+	mw.IpInfo.IpInfoWidget.prototype.displayError = function ( label ) {
+		this.$element.append(
+			new OO.ui.MessageWidget( {
+				type: 'error',
+				label: label || mw.msg( 'ipinfo-widget-error-default' ),
+				inline: true,
+				classes: [ 'ext-ipinfo-widget-error' ]
+			} ).$element
+		);
 	};
 
 	/**
