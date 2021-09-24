fc102bcf9fea8e7851ab6807de434d05d0f4d00c
Sam Smith <phuedx@wikimedia.org>
2021-09-24 14:05:22 +0100

    blockInfoRetriever: Use BlockManager::getIPBlock()
    Use \MediaWiki\Block\BlockManager::getIpBlock() to get the blocks that
    apply to the IP address.
    
    Supporting changes:
    
    * Remove MediaWiki\IPInfo\Info\BlockInfo::getNumPastBlocks as it is not
      possible to fetch expired blocks from BlockManager (or DatabaseBlock
      directly). This will be addressed in a follow up patch. See
      T270318#7377014 for detail.
    
    Bug: T270318
    Depends-On: I2009c3a7143c10167461e1ce9d4b8efe81390ff6
    Change-Id: Ibc51e4c2268f635244080e1d8f4e9a4e5d6511a0

--- tests/phpunit/unit/InfoRetriever/BlockInfoRetrieverTest.php
+++ tests/phpunit/unit/InfoRetriever/BlockInfoRetrieverTest.php
@@ -17,39 +17,26 @@ use Wikimedia\Rdbms\IDatabase;
  */
 class BlockInfoRetrieverTest extends MediaWikiUnitTestCase {
 	public function provideRetrieveFromIP(): Generator {
-		yield [ null, 0, 0, 0, 0 ];
-		yield [ null, 1, 0, 0, 1 ];
+		yield [ null, 0 ];
 
 		$block = $this->createMock( AbstractBlock::class );
 
-		yield [ $block, 1, 0, 1, 0 ];
-		yield [ $block, 2, 0, 1, 1 ];
+		yield [ $block, 1 ];
 
 		$block2 = $this->createMock( AbstractBlock::class );
 		$block3 = new CompositeBlock( [
 			'originalBlocks' => [ $block, $block2 ]
 		] );
 
-		yield [ $block3, 3, 0, 2, 1 ];
-
-		// ---
-		//
-		// Handling of unblock rows in the logging table.
-
-		yield [ null, 2, 2, 0, 0 ];
-		yield [ null, 2, 1, 0, 1 ];
-		yield [ $block, 3, 1, 1, 1 ];
+		yield [ $block3, 2 ];
 	}
 
 	/**
 	 * @dataProvider provideRetrieveFromIP
 	 */
 	public function testRetrieveFromIP(
-		$userBlock,
-		$numLoggingBlockRows,
-		$numLoggingUnblockRows,
-		$expectedNumActiveBlocks,
-		$expectedNumPastBlocks
+		$block,
+		$expectedNumActiveBlocks
 	) {
 		$ip = '127.0.0.1';
 
@@ -57,43 +44,14 @@ class BlockInfoRetrieverTest extends MediaWikiUnitTestCase {
 		$database = $this->createMock( IDatabase::class );
 
 		$blockManager->expects( $this->once() )
-			->method( 'getUserBlock' )
-			->willReturn( $userBlock );
-
-		$database->expects( $this->at( 0 ) )
-			->method( 'selectRowCount' )
-			->with(
-				'logging',
-				'*',
-				[
-					'log_type' => 'block',
-					'log_action' => 'block',
-					'log_namespace' => NS_USER,
-					'log_title' => $ip,
-				]
-			)
-			->willReturn( $numLoggingBlockRows );
-
-		$database->expects( $this->at( 1 ) )
-			->method( 'selectRowCount' )
-			->with(
-				'logging',
-				'*',
-				[
-					'log_type' => 'block',
-					'log_action' => 'unblock',
-					'log_namespace' => NS_USER,
-					'log_title' => $ip,
-				]
-			)
-			->willReturn( $numLoggingUnblockRows );
+			->method( 'getIpBlock' )
+			->willReturn( $block );
 
 		$retriever = new BlockInfoRetriever( $blockManager, $database );
 		$info = $retriever->retrieveFromIP( $ip );
 
 		$this->assertInstanceOf( BlockInfo::class, $info );
 		$this->assertEquals( $expectedNumActiveBlocks, $info->getNumActiveBlocks() );
-		$this->assertEquals( $expectedNumPastBlocks, $info->getNumPastBlocks() );
 	}
 
 }
