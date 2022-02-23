c7d8b8ab300cd3ac76b9bca3e9fbed39933f6073
STran <stran@wikimedia.org>
2022-02-23 06:48:45 -0800

    Update ipinfo log line copy
    - makes update to view_infobox and view_popup log types
    - change_access already resolved by 283380e
    
    Bug: T302000
    Change-Id: Ice1d64cfeb4c8eae9e09f7392d64e4c73bea4d8b

--- src/Logging/IPInfoLogFormatter.php
+++ src/Logging/IPInfoLogFormatter.php
@@ -14,20 +14,24 @@ class IPInfoLogFormatter extends LogFormatter {
 	protected function getMessageParameters() {
 		$params = parent::getMessageParameters();
 
-		// Replace IP user page link to IP contributions page link.
-		// Don't use LogFormatter::makeUserLink, because that adds tools links.
-		$ip = $this->entry->getTarget()->getText();
-		$userFactory = MediaWikiServices::getInstance()->getUserFactory();
-		$params[2] = Message::rawParam(
-			Linker::userLink( 0, $userFactory->newAnonymous( $ip ) )
-		);
+		if (
+			$this->entry->getSubtype() === 'view_infobox' ||
+			$this->entry->getSubtype() === 'view_popup'
+		) {
+			// Replace IP user page link to IP contributions page link.
+			// Don't use LogFormatter::makeUserLink, because that adds tools links.
+			$ip = $this->entry->getTarget()->getText();
+			$userFactory = MediaWikiServices::getInstance()->getUserFactory();
+			$params[2] = Message::rawParam(
+				Linker::userLink( 0, $userFactory->newAnonymous( $ip ) )
+			);
 
-		// Replace access level parameter with message
-		// Message keys used:
-		// - 'ipinfo-log-access-level-ipinfo-view-basic'
-		// - 'ipinfo-log-access-level-ipinfo-view-full'
-		$params[3] = $this->msg( 'ipinfo-log-access-level-' . $params[3] );
-
+			// Replace access level parameter with message
+			// Message keys used:
+			// - 'ipinfo-log-access-level-ipinfo-view-basic'
+			// - 'ipinfo-log-access-level-ipinfo-view-full'
+			$params[3] = $this->msg( 'ipinfo-log-access-level-' . $params[3] );
+		}
 		return $params;
 	}
 }
