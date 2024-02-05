a8176c836c235a312a423911df23a41d52f340f7
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-02-05 14:09:43 +0000

    Mark SchemaHandler as ignored for code coverage
    Why:
    * The SchemaHandler runs DB updates for IPInfo, but the test
      coverage reports no tests.
    * In a similar way to CheckUser and MediaModeration, code coverage
      should be ignored for this code as CI tests this when setting up
      MediaWiki via update.php.
    
    What:
    * Add @codeCoverageIgnore to SchemaHandler::onLoadExtensio
      SchemaUpdates.
    
    Bug: T356639
    Change-Id: I3870c8b4411043b52b4f181f40067a659f7a5e16

--- src/HookHandler/SchemaHandler.php
+++ src/HookHandler/SchemaHandler.php
@@ -5,7 +5,10 @@ namespace MediaWiki\IPInfo\HookHandler;
 use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 
 class SchemaHandler implements LoadExtensionSchemaUpdatesHook {
-	/** @inheritDoc */
+	/**
+	 * @inheritDoc
+	 * @codeCoverageIgnore This is tested by installing or updating MediaWiki
+	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
 		$updater->dropExtensionTable( 'ipinfo_ip_changes' );
 	}
