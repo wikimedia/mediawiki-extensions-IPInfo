76ee79eabc191ed0cee835ce32e01e7cf481c43b
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-02-16 14:12:26 +0000

    Make second successful request to REST API return a good promise
    Why:
    * The IPInfo REST APIs introduced the requirement for a CSRF token
      to prevent CSRF attacks.
    * If the token was invalid on the first request, the code is
      configured to re-attempt the request after getting a new CSRF
      token. However, if this second request is a success the code
      will not return a promise that is marked as a success.
    
    What:
    * Use a $.Deferred() as the promise to return instead of the
      promise from api.getToken() in postToRestApi() of rest.js.
    * Add success and fail handlers which will reject or resolve the
      $.Deferred() promise returned by postToRestApi.
    
    Bug: T356183
    Change-Id: I32d1aae3a7f2654acbd978486de3c4eb8293dd58
    (cherry picked from commit d433c7f17590076ef4ffe861a6a59c8b0a88e6e9)

--- modules/ext.ipInfo/rest.js
+++ modules/ext.ipInfo/rest.js
@@ -4,27 +4,45 @@ function postToRestApi( type, id, dataContext, retryOnTokenMismatch ) {
 	}
 	var restApi = new mw.Rest();
 	var api = new mw.Api();
-	return api.getToken( 'csrf' ).then( function ( token ) {
-		return restApi.post(
+	var deferred = $.Deferred();
+	api.getToken( 'csrf' ).then( function ( token ) {
+		restApi.post(
 			'/ipinfo/v0/' +
 			type + '/' + id +
 			'?dataContext=' + dataContext +
 			'&language=' + mw.config.values.wgUserLanguage,
 			{ token: token }
-		).fail( function ( _err, errObject ) {
-			if (
-				retryOnTokenMismatch &&
-				errObject.xhr &&
-				errObject.xhr.responseJSON &&
-				errObject.xhr.responseJSON.errorKey &&
-				errObject.xhr.responseJSON.errorKey === 'rest-badtoken'
-			) {
-				// The CSRF token has expired. Retry the POST with a new token.
-				api.badToken( 'csrf' );
-				return postToRestApi( type, id, dataContext, false );
+		).then(
+			function ( data ) {
+				deferred.resolve( data );
+			},
+			function ( err, errObject ) {
+				if (
+					retryOnTokenMismatch &&
+					errObject.xhr &&
+					errObject.xhr.responseJSON &&
+					errObject.xhr.responseJSON.errorKey &&
+					errObject.xhr.responseJSON.errorKey === 'rest-badtoken'
+				) {
+					// The CSRF token has expired. Retry the POST with a new token.
+					api.badToken( 'csrf' );
+					postToRestApi( type, id, dataContext, false ).then(
+						function ( data ) {
+							deferred.resolve( data );
+						},
+						function ( secondRequestErr, secondRequestErrObject ) {
+							deferred.reject( secondRequestErr, secondRequestErrObject );
+						}
+					);
+				} else {
+					deferred.reject( err, errObject );
+				}
 			}
-		} );
+		);
+	} ).fail( function ( err, errObject ) {
+		deferred.reject( err, errObject );
 	} );
+	return deferred.promise();
 }
 
 module.exports = postToRestApi;
