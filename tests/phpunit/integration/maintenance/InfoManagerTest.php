58c8be61e8dd0a3e0e3d99d0dae296d82905c346
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-05-28 17:18:18 +0100

    Test InfoManager maintenance script
    Why:
    * The InfoManager maintenance script added in
      I3e19cb1138c1f4bb4f04b86691a7edad8fbf0228 is currently untested.
    * Testing the maintenance script will ensure that the test coverage
      remains very high.
    
    What:
    * Add InfoManagerTest which tests that the data returned by the
      MediaWiki\IPInfo\InfoManager service is outputted to the console
      in a JSON format.
    * [misc] Add the maintenance script to the 'AutoloadClasses'
      property of extension.json to ensure that it gets auto-loaded.
    
    Change-Id: Iad51c110e0a675d8ee441b2a618a781a7834654b

---
+++ tests/phpunit/integration/maintenance/InfoManagerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MediaWiki\IPInfo\Test\Integration\Maintenance;
+
+use MediaWiki\IPInfo\Maintenance\InfoManager;
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+
+/**
+ * @covers \MediaWiki\IPInfo\Maintenance\InfoManager
+ */
+class InfoManagerTest extends MaintenanceBaseTestCase {
+
+	protected function getMaintenanceClass() {
+		return InfoManager::class;
+	}
+
+	public function testExecute() {
+		// Mock the IPInfoInfoManager service to return some fake data
+		$ipInfoManager = $this->createMock( \MediaWiki\IPInfo\InfoManager::class );
+		$ipInfoManager->method( 'retrieveFromIP' )
+			->with( '1.2.3.4' )
+			->willReturn( [ 'subject' => '1.2.3.4', 'data' => [ 'test' ] ] );
+		$this->setService( 'IPInfoInfoManager', $ipInfoManager );
+		// Run the maintenance script
+		$this->maintenance->setOption( 'ip', '1.2.3.4' );
+		$this->maintenance->execute();
+		$this->expectOutputString( '{"subject":"1.2.3.4","data":["test"]}' );
+	}
+}
