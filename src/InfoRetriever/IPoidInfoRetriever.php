0ced09320146b8c477523896d940159fff0c4129
Máté Szabó <mszabo@wikimedia.org>
2024-09-05 22:51:21 +0200

    Implement batch retrieval for IPoidInfoRetriever
    Why:
    
    - Special:IPInfo needs to retrieve IP information from IPoid for every
      distinct IP address used by a temporary user.
    - This requires one HTTP request per IP, which would be prohibitively
      slow to perform sequentially.
    
    What:
    
    - Interleave the requests using MultiHttpClient via the new
      retrieveBatch() method introduced in
      I07f0f2d8e2697674068ac719cd63c35a2d34408c.
    
    Bug: T349534
    Change-Id: I2dc2cba08f3c65240cb9c639fa4d0d01c0916de9

--- src/InfoRetriever/IPoidInfoRetriever.php
+++ src/InfoRetriever/IPoidInfoRetriever.php
@@ -51,28 +51,29 @@ class IPoidInfoRetriever extends BaseInfoRetriever {
 			return new IPoidInfo();
 		}
 
-		$info = array_fill_keys(
-			[
-				'behaviors',
-				'risks',
-				'connectionTypes',
-				'tunnelOperators',
-				'proxies',
-				'numUsersOnThisIP',
-			],
-			null
-		);
-
 		if ( $this->options->get( 'IPInfoIpoidUrl' ) ) {
 			$data = $this->getData( $ip );
 
-			$info['behaviors'] = $data['behaviors'] ?? null;
-			$info['risks'] = $data['risks'] ?? null;
-			$info['connectionTypes'] = $data['types'] ?? null;
-			$info['tunnelOperators'] = $data['tunnels'] ?? null;
-			$info['proxies'] = $data['proxies'] ?? null;
-			$info['numUsersOnThisIP'] = $data['client_count'] ?? null;
+			return self::makeIPoidInfo( $data );
 		}
+
+		return new IPoidInfo();
+	}
+
+	/**
+	 * Convert raw data from the IPoid service into an {@link IPoidInfo} object.
+	 * @param array $data IP data returned by IPInfo
+	 * @return IPoidInfo
+	 */
+	private static function makeIPoidInfo( array $data ): IPoidInfo {
+		$info = [
+			'behaviors' => $data['behaviors'] ?? null,
+			'risks' => $data['risks'] ?? null,
+			'connectionTypes' => $data['types'] ?? null,
+			'tunnelOperators' => $data['tunnels'] ?? null,
+			'proxies' => $data['proxies'] ?? null,
+			'numUsersOnThisIP' => $data['client_count'] ?? null,
+		];
 
 		return new IPoidInfo(
 			$info['behaviors'],
@@ -85,36 +86,91 @@ class IPoidInfoRetriever extends BaseInfoRetriever {
 	}
 
 	/**
+	 * Retrieve IP information for the given IPs from the IPoid service.
+	 * @param UserIdentity $user
+	 * @param string[] $ips IP addresses in human-readable form
+	 * @return IPoidInfo[] Map of IPoidInfo instances keyed by IP address
+	 */
+	public function retrieveBatch( UserIdentity $user, array $ips ): array {
+		$reqs = [];
+
+		foreach ( $ips as $ip ) {
+			$reqs[] = [
+				'url' => $this->getFeedEndpointUrl( $ip ),
+				'method' => 'GET'
+			];
+		}
+
+		$httpClient = $this->httpRequestFactory->createMultiClient();
+		$reqs = $httpClient->runMulti( $reqs );
+		$infoByIp = [];
+
+		foreach ( $reqs as $i => $req ) {
+			$ip = $ips[$i];
+			if ( $req['response']['code'] === 200 ) {
+				$data = $this->processRequestBody( $ip, $req['response']['body'] );
+				$infoByIp[$ip] = self::makeIPoidInfo( $data );
+			} else {
+				$infoByIp[$ip] = new IPoidInfo();
+			}
+		}
+
+		return $infoByIp;
+	}
+
+	/**
 	 * Call the iPoid API to get data for an IP address.
 	 *
 	 * @param string $ip
 	 * @return mixed[] Data returned by iPoid
 	 */
 	private function getData( string $ip ): array {
-		$data = [];
-
-		$baseUrl = $this->options->get( 'IPInfoIpoidUrl' );
-		$url = $baseUrl . '/feed/v1/ip/' . $ip;
+		$url = $this->getFeedEndpointUrl( $ip );
 		$request = $this->httpRequestFactory->create( $url, [ 'method' => 'GET' ] );
 		$response = $request->execute();
 
 		if ( $response->isOK() ) {
-			$content = json_decode( $request->getContent(), true );
-			if ( is_array( $content ) ) {
-				$sanitizedIp = IPUtils::sanitizeIP( $ip );
-				foreach ( $content as $key => $value ) {
-					if ( $sanitizedIp === IPUtils::sanitizeIP( $key ) ) {
-						$data = $value;
-					}
-				}
-			} else {
-				$this->logger->debug(
-					"ipoid results were not in the expected format: " . $request->getContent()
-				);
-			}
+			return $this->processRequestBody( $ip, $request->getContent() );
 		}
 
-		return $data;
+		return [];
+	}
+
+	/**
+	 * Get the IPoid feed endpoint URL for looking up IP data.
+	 * @param string $ip The IP to look up data for
+	 * @return string Fully qualified endpoint URL
+	 */
+	private function getFeedEndpointUrl( string $ip ): string {
+		$baseUrl = $this->options->get( 'IPInfoIpoidUrl' );
+		return $baseUrl . '/feed/v1/ip/' . $ip;
+	}
+
+	/**
+	 * Parse the given HTTP request body, which is assumed to hold data for the given IP.
+	 *
+	 * @param string $ip The IP to fetch data for
+	 * @param string $body HTTP response body
+	 * @return array Parsed response data for the given IP, or empty array if no data was available.
+	 */
+	private function processRequestBody( string $ip, string $body ): array {
+		$content = json_decode( $body, true );
+		if ( is_array( $content ) ) {
+			$sanitizedIp = IPUtils::sanitizeIP( $ip );
+			foreach ( $content as $key => $value ) {
+				if ( $sanitizedIp === IPUtils::sanitizeIP( $key ) ) {
+					return $value;
+				}
+			}
+
+			return [];
+		}
+
+		$this->logger->debug(
+			"ipoid results were not in the expected format: " . $body
+		);
+
+		return [];
 	}
 
 }
