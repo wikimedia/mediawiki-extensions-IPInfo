e2820b7457efa2aca13a4ac805ca20a2c77c332e
Pppery <mapreader@olum.org>
2024-09-05 23:57:15 -0400

    Point to MediaWiki.org not Meta
    Change-Id: I612349ae2b7a1d71d6ce333017dabb9ca819fef5

--- src/HookHandler/BetaFeaturePreferencesHandler.php
+++ src/HookHandler/BetaFeaturePreferencesHandler.php
@@ -29,9 +29,9 @@ class BetaFeaturePreferencesHandler {
 		if (
 			$this->permissionManager->userHasRight( $user, 'ipinfo' )
 		) {
-			$url = "https://meta.wikimedia.org/wiki/";
-			$infoLink = $url . "IP_Editing:_Privacy_Enhancement_and_Abuse_Mitigation/IP_Info_feature";
-			$discussionLink = $url . "Talk:IP_Editing:_Privacy_Enhancement_and_Abuse_Mitigation/IP_Info_feature";
+			$url = "https://www.mediawiki.org/wiki/";
+			$infoLink = $url . "Trust_and_Safety_Product/IP_Info";
+			$discussionLink = $url . "Talk:Trust_and_Safety_Product/IP_Info";
 
 			$betaPrefs['ipinfo-beta-feature-enable'] = [
 				'label-message' => 'ipinfo-beta-feature-title',
