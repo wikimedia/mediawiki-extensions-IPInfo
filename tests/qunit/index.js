e571503d038f15fbe79949b1ce9a3eb6ba28609b
Máté Szabó <mszabo@wikimedia.org>
2025-04-05 00:46:12 +0200

    popup: Skip rendering for blocked users with IP reveal
    Why:
    
    - We currently render the IPInfo popup widget even if the current user is
      affected by a sitewide block. Attempting to access IP information
      shows an error in this case.
    - We would like to have a setup where the popup shows formatted block
      information instead. If CheckUser is available and the current user
      has IP reveal rights, it takes care of rendering such a widget next to
      temporary account links as part of its own zero-state UI.
    - We should therefore skip rendering the IPInfo popup next to temporary
      account links if CheckUser is available, the current user is blocked
      and has IP reveal rights.
    
    What:
    
    - Skip rendering IPInfo popups next to temporary account links if
      wgCheckUserIsPerformerBlocked is true.
      I8d9dd0dc24fe9ec04c48af3d7c3e5b6cbc8f168d handles introducing this
      config var.
    - Add QUnit tests for checking when a popup is rendered.
    - A followup patchset will update the IPInfo popup to display block
      information if the current user is blocked for parity with CheckUser.
    
    Bug: T345639
    Change-Id: Ia5ceabc6469b24951ad35c958f6b1900f1c5cf8a

--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -5,3 +5,4 @@ QUnit.dump.maxDepth = 999;
 // List all test files here.
 require( './ext.ipInfo/rest.test.js' );
 require( './ext.ipInfo/infobox/widget.test.js' );
+require( './ext.ipInfo/popup/init.test.js' );
