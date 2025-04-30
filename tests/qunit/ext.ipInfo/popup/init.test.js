d9a7198448bf64ad3d51b622fed72431925fa9a2
Máté Szabó <mszabo@wikimedia.org>
2025-04-30 14:38:11 +0200

    popup: Fix target user name for expired temporary account links
    Why:
    
    - Ib7512831504aa2b51992588c27aaa196871f79df changed the HTML structure
      of expired temporary account links: there is now additional text
      content within the link, not just the user name.
    - IPInfo assumes that the text content of user links will always be the
      user name, causing it to expect an incorrect user name for expired
      temporary account links.
    - We would like to not show IP information for expired temporary account
      links at all, because it may be outdated (in the infobox) or
      unavailable due to the data having been pruned from CU, but we still
      need to expect the correct user name to be able to show the right
      error message.
    
    What:
    
    - Obtain the user name from the "data-mw-target" attribute for temporary
      account links.
    - Simplify tests and add coverage around external user links.
    
    Bug: T393002
    Change-Id: I590ef4171d251e8de89ce6a28ac96caadd6d95d9
    (cherry picked from commit 9b42cac592a9321c0bd28eec3ff09c4ee33a05ba)

--- tests/qunit/ext.ipInfo/popup/init.test.js
+++ tests/qunit/ext.ipInfo/popup/init.test.js
@@ -13,137 +13,83 @@ QUnit.module( 'ext.ipInfo.popup.index', QUnit.newMwEnvironment( {
 } ) );
 
 /**
- * Convenience function to create something resembling real MediaWiki user links
- * from a list of usernames.
+ * Convenience function to create something resembling a real MediaWiki user link
+ * from a username.
  *
- * @param {string[]} userNames
+ * @param {string} userName
  * @return {jQuery[]}
  */
-const createUserLinks = ( userNames ) => userNames.map( ( userName ) => {
+const createUserLink = ( userName ) => {
+	if ( userName.startsWith( '>' ) ) {
+		return $( '<span>' )
+			.addClass( 'mw-userlink mw-anonuserlink' )
+			.text( userName );
+	}
+
 	const $userLink = $( '<a>' )
 		.addClass( 'mw-userlink' )
 		.text( userName );
 
 	if ( mw.util.isTemporaryUser( userName ) ) {
-		$userLink.addClass( 'mw-tempuserlink' );
+		$userLink.addClass( 'mw-tempuserlink' )
+			.attr( 'data-mw-target', userName );
 	} else if ( mw.util.isIPAddress( userName ) ) {
 		$userLink.addClass( 'mw-anonuserlink' );
 	}
 
 	return $userLink;
-} );
+};
 
-const testCases = {
+const pagerTypeTestCases = {
 	'revision rows': [ 'data-mw-revid' ],
 	'log entries': [ 'data-mw-logid' ]
 };
 
-Object.entries( testCases ).forEach( ( [ testName, params ] ) => {
-	const [ dataAttr ] = params;
+for ( const [ pagerType, [ dataAttr ] ] of Object.entries( pagerTypeTestCases ) ) {
+	const testCases = {
+		'registered user link': [ 'TestUser', false, false, false ],
+		'temporary user link': [ '~1', false, true, true ],
+		'temporary user link with blocked performer': [ '~1', true, true, false ],
+		'IP user link': [ '127.0.0.1', false, true, true ],
+		'IP user link with blocked performer': [ '127.0.0.1', false, true, true ],
+		'external user link': [ '>ExternalUser', false, false, false ]
+	};
 
-	QUnit.test( `adds IPInfo button to temporary and anonymous user links in ${ testName }`, function ( assert ) {
-		// eslint-disable-next-line no-jquery/no-global-selector
-		const $content = $( '#qunit-fixture' );
-
-		this.isTemporaryUser.withArgs( sinon.match.same( '~1' ) )
-			.returns( true );
-		this.isTemporaryUser.withArgs( sinon.match.same( '127.0.0.1' ).or( sinon.match.same( 'TestUser' ) ) )
-			.returns( false );
-		this.mwConfigGet.withArgs( 'wgCheckUserIsPerformerBlocked' )
-			.returns( false );
-
-		const rows = createUserLinks( [ '~1', '127.0.0.1', 'TestUser' ] )
-			.map( ( $userLink, i ) => $( '<span>' ).attr( dataAttr, i + 1 ).append( $userLink ) );
-
-		rows.forEach( ( $row ) => $content.append( $row ) );
-
-		mw.hook( 'wikipage.content' ).fire( $content );
-
-		rows.forEach( ( $row ) => {
-			const $ipInfoButton = $row.find( '.ext-ipinfo-button' );
-			const $userLink = $row.find( '.mw-userlink' );
-			const userName = $userLink.text();
-
-			if ( userName === 'TestUser' ) {
-				assert.strictEqual( $ipInfoButton.length, 0, 'IPInfo button is not added for registered user link' );
-
-				assert.strictEqual(
-					// eslint-disable-next-line no-jquery/no-class-state
-					$userLink.hasClass( 'ext-ipinfo-anonuserlink-loaded' ),
-					false,
-					'Registered user link should not be marked as processed'
-				);
-			} else {
-				assert.strictEqual(
-					$ipInfoButton.length,
-					1,
-					'IPInfo button is added for temporary and anonymous user links'
-				);
-
-				assert.strictEqual(
-					// eslint-disable-next-line no-jquery/no-class-state
-					$userLink.hasClass( 'ext-ipinfo-anonuserlink-loaded' ),
-					true,
-					'Temporary and anonymous user links should be marked as processed'
-				);
-			}
-		} );
-	} );
-
-	QUnit.test( `adds IPInfo button to anonymous user links only in ${ testName } for blocked user with CU`,
-		function ( assert ) {
+	for ( const [
+		testName, [ userName, isPerformedBlocked, shouldMarkProcessed, shouldAddButton ]
+	] of Object.entries( testCases ) ) {
+		QUnit.test( `${ testName } in ${ pagerType }`, function ( assert ) {
 			// eslint-disable-next-line no-jquery/no-global-selector
 			const $content = $( '#qunit-fixture' );
 
 			this.isTemporaryUser.withArgs( sinon.match.same( '~1' ) )
 				.returns( true );
-			this.isTemporaryUser.withArgs( sinon.match.same( '127.0.0.1' ).or( sinon.match.same( 'TestUser' ) ) )
+			this.isTemporaryUser.withArgs( sinon.match.in( [ '127.0.0.1', '>ExternalUser', 'TestUser' ] ) )
 				.returns( false );
 			this.mwConfigGet.withArgs( 'wgCheckUserIsPerformerBlocked' )
-				.returns( true );
+				.returns( isPerformedBlocked );
 
-			const rows = createUserLinks( [ '~1', '127.0.0.1', 'TestUser' ] )
-				.map( ( $userLink, i ) => $( '<span>' ).attr( 'data-mw-revid', i + 1 ).append( $userLink ) );
-
-			rows.forEach( ( $row ) => $content.append( $row ) );
+			const $userLink = createUserLink( userName );
+			const $row = $( '<span>' )
+				.attr( dataAttr, '1' )
+				.append( $userLink )
+				.appendTo( $content );
 
 			mw.hook( 'wikipage.content' ).fire( $content );
 
-			rows.forEach( ( $row ) => {
-				const $ipInfoButton = $row.find( '.ext-ipinfo-button' );
-				const $userLink = $row.find( '.mw-userlink' );
-				const userName = $userLink.text();
+			assert.strictEqual(
+				$row.find( '.ext-ipinfo-button' ).length,
+				shouldAddButton ? 1 : 0,
+				`IPInfo button should ${ shouldAddButton ? 'be' : 'not be' } added for ${ testName }`
+			);
 
-				if ( userName === 'TestUser' ) {
-					assert.strictEqual(
-						// eslint-disable-next-line no-jquery/no-class-state
-						$userLink.hasClass( 'ext-ipinfo-anonuserlink-loaded' ),
-						false,
-						'Registered user link should not be marked as processed'
-					);
-				} else {
-					assert.strictEqual(
-						// eslint-disable-next-line no-jquery/no-class-state
-						$userLink.hasClass( 'ext-ipinfo-anonuserlink-loaded' ),
-						true,
-						'Temporary and anonymous user links should be marked as processed'
-					);
-				}
+			assert.strictEqual(
+				// eslint-disable-next-line no-jquery/no-class-state
+				$userLink.hasClass( 'ext-ipinfo-anonuserlink-loaded' ),
+				shouldMarkProcessed,
+				`${ testName } should ${ shouldMarkProcessed ? 'be' : 'not be' } marked as processed`
+			);
+		} );
 
-				if ( userName === '127.0.0.1' ) {
-					assert.strictEqual(
-						$ipInfoButton.length,
-						1,
-						'IPInfo button is added for anonymous user links only'
-					);
-				} else {
-					assert.strictEqual(
-						$ipInfoButton.length,
-						0,
-						'IPInfo button is not added for registered or temporary user links'
-					);
-				}
-			} );
-		}
-	);
-} );
+	}
+}
